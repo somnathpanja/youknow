cba6c1c204c21df0890ec069eea3565d3c5fb0b6
somnathpanja <somnathpanja@gmail.com>
2015-01-18 07:30:58 +0530

    New files added

--- worker/worker.js
+++ worker/worker.js
@@ -30,7 +30,7 @@
         res.send(200); // Return before start computing
 
         // Do some CPU computation
-        for (var i = 0; i < 10000; i++) {
+        for (var i = 0; i < 100000; i++) {
             console.log(i * Math.log(i));
         }
     });
