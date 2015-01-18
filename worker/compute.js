ca8f2614e0fffc8dafae8b26cbfa75868818d5f5
somnathpanja <somnathpanja@gmail.com>
2015-01-18 08:15:44 +0530

    New files added

--- worker/compute.js
+++ worker/compute.js
@@ -5,9 +5,36 @@
 
 (function (module) {
 
-   var xx = 0;
-   for(var i=0; i< 100000000000000000; i++){
-        xx = xx * (i %3 ) /1222;
-   }
+    function doheavyWork() {
+
+        this.elementsLength = 1000; // Amount of operations
+        this.currentPosition = 0; // Current position
+
+        // Initializer to start the iterator
+        this.startCalculation = function() {
+            // Reset current position to zero
+            this.currentPosition = 0;
+            // Start looping
+            setTimeout(
+                this.calculate.bind(this),
+                0
+            );
+        };
+
+        this.calculate = function(){
+            // Check that we still have iterations left, otherwise, return
+            // out of function without calling a new one.
+            if( this.currentPosition > this.elementsLength ) return;
+            // Do computation
+            doHeavyLifting( element[ this.currentPosition ] );
+
+            // Add to counter
+            this.currentPosition++;
+        }
+    }
+
+    // Initalize the object and start iterating
+    var computationIterator = new doheavyWork();
+    computationIterator.startCalculation();
 
 })(module);
