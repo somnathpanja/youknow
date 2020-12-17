5eeef4f598c7d1e79a56fed2ba440f47d3374595
Somnath Panja <somnath.panja@cambiumnetworks.com>
2020-12-18 00:13:08 +0530

    cleanup

--- monitor/client/youknow/src/app/server.ts
+++
@@ -1,9 +0,0 @@
-export interface Server {
-    agent_id: String,
-    ip: String,
-    cpu_count: Number,
-    platform: String,
-    stats_interval_ms: Number,
-    last_updated_ts: Number,
-    watch_process: String[]
-}
