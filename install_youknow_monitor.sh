f35252e1fda3c3977cd4130d7ade3332df531bf6
Somnath Panja <somnath.panja@cambiumnetworks.com>
2021-02-25 04:19:38 +0530

    script updated

--- install_youknow_monitor.sh
+++ install_youknow_monitor.sh
@@ -1,8 +1,7 @@
   wget -q https://codeload.github.com/somnathpanja/youknow/zip/master -O youknow-master.zip
-  sudo apt-get install unzip
-  unzip youknow-master.zip
+  unzip -o youknow-master.zip
   cd youknow-master/monitor
-  npm install
-  cd ..
+  sudo npm install
+  
 
   
